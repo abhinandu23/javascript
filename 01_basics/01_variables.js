66d89050048f7ddbdc683089909729a96921a340
abhinandu23 <161216563+abhinandu23@users.noreply.github.com>
2024-02-25 13:32:38 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=14423
+let accountEmail="nandupostbank@gmail.com"
+var accountPassword = "12345"
+let accountState;
+
+accountEmail="abhi@gmail.com"
+accountPassword = "1234232"
+accountCity  = "Hyderabad"
+console.log([accountEmail,accountPassword,accountCity]);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+console.log(4+3);
+
+let name = "abhi"
+let age = 20
+
+
+//number
+//bigint
+//strung
+//boolean => true/false
+//null => standlone value means empty value
+//undefined => didnt assigned value tillnow
+//symbol =>unique
+
+
+//object
+console.log(typeof "abhi"); //op=string
+console.log(typeof null); //op=object
+console.log(typeof undefined); //op=undefined
